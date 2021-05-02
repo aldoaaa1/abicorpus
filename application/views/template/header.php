31647c01516b32227bfa1d4b7c3f5c2aa725bae0
Aldo Alcalá <aldo.aaa1@gmail.com>
2021-05-02 17:28:40 -0500

    Acondicionamiento de BaseUrl()
    variable en php y Javascript

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<title><?= $title ?></title>
 	<meta name="description" content="<?= $description ?>">
 	<meta name="keywords" content="<?= $keywords ?>">
@@ -10,12 +10,12 @@
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>fonts/Roboto/roboto.css">
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>fonts/BootstrapIcons/bootstrap-icons.css">
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>fonts/MaterialIcons/Material_Icons.css">
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>plugin/materialize.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="<?=base_url()?>plugin/materialize.min.css"> -->
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>plugin/sweetalert2.min.css">
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/general.css">
 	<?php
 		if (file_exists('css/'.$page.'.css')) {
-			echo '<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/'.$page.'.css">';
+			echo '<link rel="stylesheet" type="text/css" href="'.base_url().'css/'.$page.'.css">';
 		}
 	?>
 </head>
--- application/views/template/titles.php
+++ application/views/template/titles.php
@@ -4,4 +4,8 @@
 	$GLOBALS['description']['index'] = 'Esta es la pagina index';
 	$GLOBALS['keywords']['index'] = 'keyword1 keyword2';
 
+	$GLOBALS['title']['agenda'] = 'Agenda';
+	$GLOBALS['description']['agenda'] = 'Agenda de clientes AbiCorpus';
+	$GLOBALS['keywords']['agenda'] = 'Agenda';
+
 ?>
